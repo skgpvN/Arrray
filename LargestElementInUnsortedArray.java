156344391a226d85f306e12d27a7d01d778856c6
Nandkishor Sheshrao Deshmukh <109015980+skgpvN@users.noreply.github.com>
2022-08-09 10:38:22 +0530

    Update LargestElementInUnsortedArray.java

--- LargestElementInUnsortedArray.java
+++ LargestElementInUnsortedArray.java
@@ -3,10 +3,10 @@ package Array;
 public class LargestElementInUnsortedArray {
     public static void main(String[] args) {
         int arr[]={1,3,4,78,67,45,2};
-        int max=0;
-        for (int i = 0; i < arr.length-1; i++) {
-           if (arr[i]< arr[i+1]){
-               max=arr[i+1];
+        int max=arr[0];
+        for (int i = 1; i < arr.length-1; i++) {
+           if (arr[i]> max){
+               max=arr[i];
 
             }
             System.out.println(max);
